fdc2bc5eedd3041f518fcfd418a095b26867ece6
Brett Lazarine <jblazarine@gmail.com>
2023-02-26 00:33:11 -0600

    complete Factory Pattern exercise 2

---
+++ FactoryPatternExercise2/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPatternExercise2
+{
+    internal class Product
+    {
+        public Product (string name, double Price)
+        {
+            this.Name = name;
+            this.Price = Price;
+        }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public void PrintProducts(IEnumerable<Product> products)
+        {
+            Console.WriteLine("Products in the database and their price:");
+            foreach (Product product in products)
+            {
+                Console.WriteLine($"{product.Name}: ${product.Price}");
+            }
+        }
+    }
+}
--- FactoryPatternExercise2/Program.cs
+++ FactoryPatternExercise2/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var p1 = new Product("battery", 1.75);
+            var p2 = new Product("water bottle", 2.25);
+            bool isDb = false;
+            string answer; 
+            do
+            {
+                Console.WriteLine("What database do you want to use: List, SQL, or Mongo?");
+                answer = Console.ReadLine();
+                if (answer.ToLower() == "list" || answer.ToLower() == "sql" || answer.ToLower() == "mongo") { isDb = true; }
+            } while (isDb == false);
+            IDataAccess dataAccess = DataAccessFactory.GetDataAccessType(answer);
+            var prodList = dataAccess.LoadData(); prodList.Add(p1); prodList.Add(p2);
+            Console.WriteLine("Products in the database and their price:");
+            foreach (Product product in prodList)
+            {
+                Console.WriteLine($"{product.Name}: ${product.Price}");
+            }
+            dataAccess.SaveData();
         }
     }
 }
